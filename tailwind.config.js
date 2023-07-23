0e526a122a56b96c431c3725316a0f0566266971
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-07-23 18:35:45 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/**/*.{blade.php,vue,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
