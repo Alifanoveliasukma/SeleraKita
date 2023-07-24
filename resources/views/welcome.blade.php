f21de7a51fd60759484975051336a5b67c840c69
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-07-24 10:23:41 +0700

    create mockup view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,196 +8,65 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" href="">
 </head>
 <body>
-    <div class="w-[120px] h-[2163px] relative bg-white shadow">
-        <div class="h-[78.01px] left-0 top-0 absolute flex-col justify-center items-center inline-flex">
-          <div class="w-[120px] h-[77px] px-8 py-6 justify-center items-center gap-2 inline-flex">
-            <div class="w-[60px] h-[63px] relative">
-              <div class="w-[60px] h-[60px] left-0 top-[3px] absolute bg-orange-500 bg-opacity-30 rounded-full"></div>
-              <div class="left-[14px] top-[4px] absolute text-neutral-800 text-5xl font-bold leading-[57.60px] tracking-wide">A</div>
-              <div class="w-[22px] h-[22px] left-[35px] top-0 absolute rounded-[48px]">
-                <div class="w-[18.82px] h-[19.39px] left-[1.59px] top-[1.03px] absolute">
-                  <div class="w-[18.82px] h-[19.39px] left-0 top-0 absolute">
-                    <div class="w-[1.14px] h-[1.24px] left-[1.64px] top-[10.44px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[7.89px] top-[14.09px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
+    <header class="bg-slate-600 p-2">
+        <nav class="container mx-auto flex items-center justify-between">
+            <!-- Logo atau Nama Website -->
+            <div class="relative">
+                <div class="w-[294px] h-11 px-4 py-2 bg-white rounded-3xl border border-neutral-200 justify-start items-center gap-2 inline-flex">
+                    <div class="w-3.5 h-3.5 relative">
+                      <div class="w-[11.08px] h-[11.08px] left-[1.75px] top-[1.17px] absolute">
+                        <div class="w-[10.62px] h-[10.37px] left-0 top-0 absolute rounded-full border border-neutral-400"></div>
                       </div>
                     </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[14.45px] top-[7.30px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[12.02px] top-[9.34px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[5.87px] top-[14.63px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[5.71px] top-[10.45px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[3.92px] top-[3.21px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[10.38px] top-[2.16px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.25px] left-[5.92px] top-[5.29px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[9.14px] top-[12.02px] absolute">
-                    </div>
-                    <div class="w-[1.14px] h-[1.24px] left-[12.78px] top-[14.83px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.94px] left-[1.43px] top-[8.30px] absolute">
-                    </div>
-                    <div class="w-[1.87px] h-[1.94px] left-[4.36px] top-[8.04px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[6.74px] top-[11.44px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[3.93px] top-[13.79px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[2.59px] top-[10.77px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[1.86px] h-[1.94px] left-[5.05px] top-[3.12px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[9.17px] top-[3.92px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[11.63px] top-[4.27px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[13.33px] top-[6.75px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[13.36px] top-[10.62px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[11.13px] top-[13.33px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.94px] left-[10px] top-[9.74px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[7px] top-[8.69px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[10.03px] top-[6.14px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[6.07px] top-[4.80px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[2.75px] top-[6.60px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[1.98px] top-[4.24px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[0.95px] h-px left-[7.32px] top-[13.63px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[5.06px] top-[6.53px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[3.37px] top-[9.19px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[10.52px] top-[12.27px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[11.01px] top-[15.92px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[15.94px] top-[10.66px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[12.79px] top-[11.60px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-[-0px] absolute">
-                      </div>
-                    </div>
-                    <div class="w-[0.95px] h-px left-[14.45px] top-[5.31px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[15.66px] top-[5.85px] absolute">
-                    </div>
-                    <div class="w-px h-px left-[6.32px] top-[1.79px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[7.40px] top-[0.87px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[0.95px] h-px left-[11.01px] top-[3.51px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[7.46px] top-[6.64px] absolute">
-                    </div>
-                    <div class="w-[1.86px] h-[1.93px] left-[11.77px] top-[1.68px] absolute">
-                    </div>
-                    <div class="w-[2.88px] h-[3.24px] left-[12.23px] top-[2.38px] absolute">
-                      <div class="w-[2.88px] h-[3.02px] left-0 top-0 absolute">
-                      </div>
-                    </div>
-                    <div class="w-[0.95px] h-px left-[6.26px] top-[8.11px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[9.28px] top-[6.13px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[7.08px] top-[3.66px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[14.40px] top-[12.48px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[15.94px] top-[8.55px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[4.17px] top-[11.79px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[2.45px] top-[5.09px] absolute">
-                    </div>
-                    <div class="w-[0.95px] h-px left-[5.51px] top-[12.34px] absolute">
-                    </div>
+                    <input type="text" class="w-60 text-neutral-400 text-base font-normal leading-7 tracking-tight outline-none" value="Search">
                   </div>
-                  <div class="w-[2.61px] h-[3.05px] left-[11.44px] top-[11.45px] absolute">
-                  </div>
-                  <div class="w-[2.45px] h-[3.16px] left-[12.54px] top-[4.18px] absolute">
-                  </div>
-                  <div class="w-[2.61px] h-[3.05px] left-[4.20px] top-[3.81px] absolute">
-                  </div>
-                  <div class="w-[1.86px] h-[3.06px] left-[8.90px] top-[7.60px] absolute">
-                  </div>
-                  <div class="w-[1.86px] h-[3.06px] left-[2.81px] top-[9.68px] absolute">
-                  </div>
-                  <div class="w-[2.61px] h-[3.06px] left-[7.86px] top-[2.58px] absolute">
-                  </div>
-                  <div class="w-[1.86px] h-[3.06px] left-[13.47px] top-[7.70px] absolute">
-                  </div>
-                  <div class="w-[2.45px] h-[3.17px] left-[6.69px] top-[14.10px] absolute">
-                  </div>
+            </div>
+            
+            <!-- Kotak di dalam Navbar -->
+            <div class="bg-slate-400 mr-8">
+                <ul class="flex space-x-4 py-0">
+                    <div class="flex items-center gap-1 p-1">
+                    <li><div class="w-6 h-6 bg-gray-900"></div></li>
+                    <li><div class="w-6 h-6 bg-gray-900"></div></li>
+                    <li><div class="w-6 h-6 bg-gray-900"></div></li>
+                    <li><div class="w-6 h-6 bg-gray-900"></div></li>
+                    <li><div class="w-11 h-11 rounded-full bg-gray-900"></div></li>
+                    <li><p class="text-base font-normal not-italic">Austin Robertson</p>
+                        <p class="text-xs font-normal not-italic">Marketing Administrator</p></li>
+                    </div>                                     
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="max-w-7xl mx-auto grid md:grid-cols-3 gap-4 mt-8 mx-8"> <!-- Container -->
+
+        <article class="md:col-span-2 bg-orange-100 p-4"> <!-- Content -->
+        
+            <p class="border border-slate-500 h-56">Content 2/3</p>
+            <p class="border border-slate-500 h-[500px] mt-4">Content 2/3</p>
+        
+            <section class="grid md:grid-cols-2 gap-4 mt-4">
+                
+
+                <div class="border border-indigo-500 mt-10">
+                Related content 1/2
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="w-[120px] h-px bg-gray-200 border-gray-200"></div>
-        </div>
-        <div class="p-6 left-[10px] top-[77px] absolute flex-col justify-center items-center gap-6 inline-flex">
-          <div class="p-4 bg-orange-500 rounded-[100px] justify-start items-start inline-flex">
-            <div class="w-5 h-5 relative">
-              <img class="w-5 h-5 left-0 top-0 absolute" src="https://via.placeholder.com/20x20" />
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="h-5 left-[10px] top-[10px] absolute">
-              <img class="w-5 h-[15.94px] left-[-0px] top-[4.06px] absolute" src="https://via.placeholder.com/20x16" />
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="w-[17px] h-5 left-[11px] top-[10px] absolute">
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="w-[22px] h-4 left-[9px] top-[12px] absolute">
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="w-4 h-5 left-[12px] top-[10px] absolute">
-              <img class="w-4 h-5 left-0 top-0 absolute" src="https://via.placeholder.com/16x20" />
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="w-[23px] h-5 left-[10px] top-[10px] absolute">
-              <img class="w-[23px] h-5 left-0 top-0 absolute" src="https://via.placeholder.com/23x20" />
-              <div class="w-[2.30px] h-[2.22px] left-[17.25px] top-[8.78px] absolute bg-neutral-400 rounded-full"></div>
-            </div>
-          </div>
-          <div class="w-10 h-10 relative bg-neutral-100 rounded-[100px]">
-            <div class="w-5 h-[18px] left-[10px] top-[11px] absolute">
-            </div>
-          </div>
-        </div>
-      </div>
-      
+                
+                <div class="border border-pink-700 mt-10">
+                Related content 1/2
+                </div>
+            
+            </section>
+        
+        </article> <!-- ./Conten -->
+        
+        <aside class="bg-green-200 p-4"> <!-- Aside -->
+        
+            <div class="border border-slate-600 h-56 bg-slate-800 ">Sidebar 1/3</div>
+            <div class="border border-slate-600 h-[500px] bg-gray-200 mt-4 ">Sidebar 1/3</div>
+        
+        </aside> <!-- ./Aside -->
+    
+    </main> <!-- ./Container-->
 </body>
 </html>
