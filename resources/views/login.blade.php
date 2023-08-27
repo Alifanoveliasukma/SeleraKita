f02af587b744e09972b1a0bd462d279cbe32b2e9
AdamAbd <adamabd14113@gmail.com>
2023-08-27 15:13:14 +0700

    add food illustration

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
 </head>
 
 <body class="container mx-auto flex items-center h-screen bg-orange-50">
-    <div class="w-[460px]">
+    <div class="w-1/3">
         <img src="./assets/logo/logo_aprycot.png" alt="Logo" class="w-[185px] h-[62px]">
         <h1 class="font-bold text-4xl text-center mt-6">Sign In</h1>
         <p class="font-normal text-base text-gray-400 text-center mt-4">Sign in to stay connected.</p>
@@ -47,16 +47,27 @@
         </form>
         <p class="font-normal text-base text-center mt-4">or sign in with other accounts?</p>
         <div class="mt-4 flex flex-row w-full justify-center gap-6">
-            <img src="./assets/logo/google.png" alt="Google Sign In" class="w-6">
-            <img src="./assets/logo/facebook.png" alt="Facebook Sign In" class="w-6">
-            <img src="./assets/logo/instagram.png" alt="Instagram Sign In" class="w-6">
-            <img src="./assets/logo/linkedin.png" alt="Linked In Sign In" class="w-6">
+            <button type="button" class="w-6">
+                <img src="./assets/logo/google.png" alt="Google Sign In">
+            </button>
+            <button type="button" class="w-6">
+                <img src="./assets/logo/facebook.png" alt="Facebook Sign In">
+            </button>
+            <button type="button" class="w-6">
+                <img src="./assets/logo/instagram.png" alt="Instagram Sign In">
+            </button>
+            <button type="button" class="w-6">
+                <img src="./assets/logo/linkedin.png" alt="Linked In Sign In">
+            </button>
         </div>
         <div class="flex flex-row justify-center gap-1">
             <p class="font-normal text-base text-center mt-4">Don’t have an account?</p>
             <button type="button" class="font-normal text-base text-orange-500 text-center mt-4">Click here to sign
                 up.</button>
         </div>
+    </div>
+    <div class="w-2/3 h-screen flex items-center justify-end">
+        <img src="./assets/illustration/food.png" alt="Food Illustration" class="w-[800px]">
     </div>
 </body>
 
